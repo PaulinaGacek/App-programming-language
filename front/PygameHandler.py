011182c5424755f62e5371aac16cb249c9d8916b
PaulinaGacek <paulina.gacek.pl@gmail.com>
2022-04-10 15:25:50 +0200

    Checking collisions corrected

--- front/PygameHandler.py
+++ front/PygameHandler.py
@@ -31,19 +31,18 @@ class PyGameHandler:
         for obj in PyGameHandler.objects:
             if obj.is_pixel_inside(x, y) is True:
                 return False
-            else:
-                return True
+        return True
 
     '''
         Checks if all points on the perimeter of new object are available.
     '''
-    def can_object_be_draw(x,y) -> bool:
+    def can_object_be_drawn(x,y) -> bool:
 
         if x-PyGameHandler.RADIUS < 0 or x+ PyGameHandler.RADIUS > PyGameHandler.WIDTH:
-            return false
+            return False
         
         if y-PyGameHandler.RADIUS < 0 or y+PyGameHandler.RADIUS > PyGameHandler.HEIGHT:
-            return false
+            return False
         
         # (x_ - x)**2 + (y_ - y)**2 = RADIUS**2
         # y_ = sqrt(RADIUS**2 - (x_ - x)**2) + y
@@ -56,10 +55,11 @@ class PyGameHandler:
             y2 = - math.sqrt(PyGameHandler.RADIUS**2 - (x2-x)**2) + y
 
             if not PyGameHandler.is_pixel_available(int(x1), int(y1)):
-                print("Pixel: ({},{}) not available".format(x1,y1))
+                print("Pixel: ({},{}) not available".format(x1,int(y1)))
                 return False
-            if not PyGameHandler.is_pixel_available(x2, y2):
-                print("Pixel: ({},{}) not available".format(x1,y1))
+
+            if not PyGameHandler.is_pixel_available(x2, int(y2)):
+                print("Pixel: ({},{}) not available".format(x2,int(y2)))
                 return False
         
         return True
@@ -71,17 +71,24 @@ class PyGameHandler:
     '''
     def add_new_object(name, x, y):
 
-        # if not PyGameHandler.can_object_be_draw(x,y):
-            # raise ObjectCannotBeDrawn(name,x,y)
+        if x is not None and not PyGameHandler.can_object_be_drawn(x,y):
+            raise ObjectCannotBeDrawn(name,x,y)
 
         object_ = Object(name, x, y, PyGameHandler.RADIUS)
         PyGameHandler.objects.append(object_)
     
 
+    '''
+        Used with 'SET' command to modify parameters of the object
+    '''
     def modify_object(name, x, y):
         for obj in PyGameHandler.objects:
             print("Name: {}".format(obj.name))
             if obj.name == name:
+
+                if not PyGameHandler.can_object_be_draw(x,y):
+                    raise ObjectCannotBeDrawn(name,x,y)
+
                 obj.x = x
                 obj.y = y
                 break
